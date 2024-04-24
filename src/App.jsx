0d2cb595a11795ae3c829ad4fa958bc46a135b30
shahnapinit <shahana.p@initsolutions.in>
2024-04-24 23:21:45 +0530

    dashboard connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
-      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+      {/* <Route path="*" element={<Navigate to="/dashboard/home" replace />} /> */}
+      <Route path="/dashboard" element ={<Navigate to="/dashboard/home" replace />}/>
+
+      <Route path="/" element ={<Login />}/>
+      <Route path="/signup" element ={<Signup/>}/>
+
+
     </Routes>
   );
 }
