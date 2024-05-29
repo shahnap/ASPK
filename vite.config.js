52733a4564f67e33be83892a32d9c1512a67f982
shahnapinit <shahana.p@initsolutions.in>
2024-05-29 22:45:08 +0530

    vercel again and again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+    alias: {
+      '@': '/src',
+      screens: '/src/screens',
+      components: '/src/components',
+    }
+  }
 });
