a3e07a5768ea954a54688188fda9e8f3258cd1ec
shahnapinit <shahana.p@initsolutions.in>
2024-08-03 15:01:33 +0530

    3

--- src/configs/config/Config.jsx
+++ src/configs/config/Config.jsx
@@ -1,4 +1,2 @@
 // export const BaseUrl = "http://localhost:5000/"
 export const BaseUrl ='https://aspk-backend-mdcp8yrww-shahnaps-projects.vercel.app'
-
-// export const BaseUrl ='https://aspk-backend-mdcp8yrww-shahnaps-projects.vercel.app'
